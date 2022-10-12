1f5e1507ad6cdcf4e176bead7f180663e3064f42
MatanNahum <matan.nahum95@gmail.com>
2022-10-12 18:00:22 +0300

    spot check done

--- solutions/check_2.sql
+++ solutions/check_2.sql
@@ -1,1 +1,5 @@
 USE sql_intro;
+
+SELECT item_purchased, amount, name, industry
+FROM transaction as tr, company as co
+WHERE tr.company_id = co.id;
--- solutions/check_4.sql
+++ solutions/check_4.sql
@@ -1,1 +1,8 @@
 USE sql_intro;
+
+SELECT s.s_name as name
+FROM Student as s, teacher as t, student_teacher as st
+WHERE 
+    t.t_name = "Foster" AND
+    s.s_id = st.student_id AND
+    t.t_id = st.teacher_id;
