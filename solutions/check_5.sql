a7d79dc66c7318433925a3fb868d243949075fab
MatanNahum <matan.nahum95@gmail.com>
2022-10-12 19:08:08 +0300

    ex1 Done

--- solutions/check_5.sql
+++ solutions/check_5.sql
@@ -3,3 +3,5 @@ USE sql_intro;
 SELECT t_name
 FROM Student as s, teacher as t, student_teacher as st
 Where s.s_name = "Leo" AND t.is_tenured IS TRUE AND s.s_id = st.student_id AND t.t_id = st.teacher_id;  
+
+
