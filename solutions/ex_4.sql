208ecad166c0199b48fbf994b5d60a8baa152b96
MatanNahum <matan.nahum95@gmail.com>
2022-10-12 19:37:17 +0300

    ex 5

--- solutions/ex_4.sql
+++ solutions/ex_4.sql
@@ -1,1 +1,6 @@
 USE sql_intro;
+
+SELECT id, survival_rate
+FROM patient, disease
+WHERE patient.disease IS NOT NULL
+ORDER BY id ASC
--- solutions/ex_5.sql
+++ solutions/ex_5.sql
@@ -1,1 +1,5 @@
 USE sql_intro;
+
+SELECT symptoms_family, COUNT(*) FROM patient
+WHERE disease = "Cabbage disease"
+GROUP BY symptoms_family
